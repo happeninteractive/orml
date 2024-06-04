0df8439dfd0cf33cdd2ef39c0d908977d9148d99
andrewart1 <130918125+andrewart1@users.noreply.github.com>
2024-06-04 11:44:18 -0400

    updated build, kotlin and depedencies versions to match other openrndr repos

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
-    var orxUseSnapshot = true
-    var openrndrUseSnapshot = true
-    var orx = if (orxUseSnapshot) "0.5.1-SNAPSHOT" else "0.4.1"
-    var openrndr = if (openrndrUseSnapshot) "0.5.1-SNAPSHOT" else "0.4.1"
-    val kotlin = "1.7.20"
+    var orxUseSnapshot = false
+    var openrndrUseSnapshot = false
+    var orx = if (orxUseSnapshot) "0.5.1-SNAPSHOT" else "0.4.4"
+    var openrndr = if (openrndrUseSnapshot) "0.5.1-SNAPSHOT" else "0.4.4"
+    val kotlin = "1.9.0"
 }
